5221646b260a59f93268faf9f7dc8d7c27ced7a8
anewport <anewport@dxc.com>
2019-11-27 14:35:01 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'serverless-stack-notes-app-uploads-ash',
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://yg3m5e4sv2.execute-api.us-east-1.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_9AAnIprf6',
+    APP_CLIENT_ID: '3acjhhbqfmni0k520oo75t6iqm',
+    IDENTITY_POOL_ID: 'us-east-1:b9d0bd58-dcc8-4656-a1ed-1d3c84c4ba8d',
+  },
+};
